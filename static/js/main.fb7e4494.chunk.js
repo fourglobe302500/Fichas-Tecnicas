012816c551c3488dc70e7befbc651ec263e8f5b6
fourglobe302500 <franciscomiguelolima@gmail.com>
2021-01-13 17:23:01 +0000

    Updates

---
+++ static/js/main.fb7e4494.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfichas_tecnicas=this.webpackJsonpfichas_tecnicas||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(0),r=c(1),s=c.n(r),i=c(3);var a=function(){return Object(n.jsx)(n.Fragment,{children:"Hello Wolrd!"})};c.n(i).a.render(Object(n.jsx)(s.a.StrictMode,{children:Object(n.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.fb7e4494.chunk.js.map
