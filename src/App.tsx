c6b26a8b70bb40b357bc381eb5572d50025a39ff
fourglobe302500 <franciscomiguelolima@gmail.com>
2021-01-14 18:26:21 +0000

    changing to material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+import { Box } from "@material-ui/core";
 import React from "react";
-import "./App.css";
+import { Content } from "./components/Content";
 import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Header content="Main Page"></Header>
-      <div className="paragraph">
-        <p>Hello this page is still in development.</p>
-      </div>
-    </div>
+    <Box
+      margin=".75rem"
+      marginTop="1.5rem"
+      padding=".75rem"
+      border={2}
+      borderColor="darkblue"
+      borderRadius="3rem"
+      color="cyan"
+      minHeight="80vh"
+      marginX="4rem"
+    >
+      <Header></Header>
+      <Content>Hello this page is under development</Content>
+    </Box>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
+import { Box } from "@material-ui/core";
 import React from "react";
-import "./Header.css";
 
 interface HeaderProps {
-  content: string;
+  content?: string;
 }
 
-export const Header: React.FC<HeaderProps> = ({ content }) => {
-  return <div className="Header">{content}</div>;
+export const Header: React.FC<HeaderProps> = ({ content = "Main Page" }) => {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      fontSize="2rem"
+      marginX="auto"
+      marginTop="1rem"
+      maxWidth="fit-content"
+      padding="1rem"
+      border={1}
+      borderColor="gray"
+      borderRadius="borderRadius"
+    >
+      {content}
+    </Box>
+  );
 };
